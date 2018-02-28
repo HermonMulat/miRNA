f3d233f7e1cf4e88cc478967de1280dafd66ddcc
Collin Epstein <collineps@collins-air.wireless.davidson.edu>
2018-02-28 17:36:08 -0500

    completed random forest test script. need to run trials.

--- random_forest.py
+++ random_forest.py
@@ -19,7 +19,7 @@ from time import time
 from sklearn.feature_selection import VarianceThreshold, SelectPercentile, chi2
 from sklearn.preprocessing import StandardScaler, binarize
 from sklearn.model_selection import RandomizedSearchCV
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import RandomForestClassifier
 
 from decision_tree import read_data, report, variance_feat_sel, chi2_feat_sel
 
@@ -36,19 +36,20 @@ def main():
         print "Data read from file"
 
         # trial settings
-        toy = False
+        toy = True
         variance_flag = False
         threshold = 0.0
-        chi2_flag = True
+        chi2_flag = False
         percentile = 10
-        binarize_flag = True
+        binarize_flag = False
 
         # quick trial on subset of training data
         if toy:
             frac = 0.2
-            print "Running on {0}% of data.".format(frac)
-            features = features[:int(len(features)*frac)]
-            targets = targets[:int(len(targets)*frac)]
+            num = int(len(features)*frac)
+            print "Running on {0}% of data, {1} features.".format(frac, num)
+            features = features[:num]
+            targets = targets[:num]
 
         # feature selection based on variance
         if variance_flag:
@@ -72,29 +73,33 @@ def main():
             print "Data standardized"
         print ""
 
-        # parameter distribution for randomized grid search
-        # NEED RANDOM FOREST PARAMETERS
-        param_dist = {"criterion" : ["gini", "entropy"], # use entropy
-                      "splitter" : ["best", "random"],
+        # parameter distribution for randomized hyperparameter search
+        param_dist = {"n_estimators" : range(0,101)[::2],
+                      "criterion" : ["gini", "entropy"],
+                      "max_features" : ["sqrt", "log2", 0.75, 0.5, 0.25, None],
                       "max_depth" : range(2,21),
                       "min_samples_split" : range(2,21),
                       "min_samples_leaf" : range(1,21),
                       #"min_weight_fraction_leaf" : [], ?
-                      "max_features" : ["sqrt", "log2", 0.75, 0.5, 0.25, None], # use None
                       #"random_state" : [], ?
                       "max_leaf_nodes" : range(10, 100)[::10] + [None],
-                      "min_impurity_decrease" : [0.0, 0.05, 0.1, 0.15, 0.2, 0.25], # use 0.0
-                      #"class_weight" : [], ?
-                      #"presort" : [True, False] ?
+                      "min_impurity_decrease" : [0.0, 0.05, 0.1, 0.15, 0.2, 0.25],
+                      "bootstrap" : [True, False],
+                      "oob_score" : [True, False],
+                      "n_jobs" : [-1], # parallelize as much as possible
+                      #"random_state" : [], ?
+                      #"verbose" : [], ?
+                      "warm_start" : [True, False],
+                      #"class_weight" : [] ?
                       }
 
         # set up trials
-        #dtc = DecisionTreeClassifier() RANDOM FOREST
+        rfc = RandomForestClassifier()
         iterations = 100
         scoring = "f1_weighted"
         folds = 10
-        # ON RANDOM FOREST
-        rscv = RandomizedSearchCV(dtc, param_distributions=param_dist,
+
+        rscv = RandomizedSearchCV(rfc, param_distributions=param_dist,
                                     n_iter=iterations, scoring=scoring, cv=folds)
 
         # print trial parameters
@@ -102,10 +107,10 @@ def main():
 
         # run randomized hyperparameter search with cross validation on decision tree
         start = time()
-        #rscv.fit(features, targets, n_jobs=1) RANDOM FOREST
+        rscv.fit(features, targets, n_jobs=-1)
         print("RandomizedSearchCV took %.2f seconds for %d candidates"
               " parameter settings." % ((time() - start), iterations))
-        #report(rscv.cv_results_) ON RANDOM FOREST
+        report(rscv.cv_results_)
         print "Trial finished\n"
 
 if __name__ == '__main__':
