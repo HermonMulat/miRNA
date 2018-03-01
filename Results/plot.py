7ad391f5b5488cf00bef853f7afff49b9c52cc62
Hermon <hermonmulat@gmail.com>
2018-03-01 03:31:37 -0500

    Add results for KNN and logistic regression models

---
+++ Results/plot.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+def read_data(fn):
+    data = []
+    with open(fn,"r") as fl:
+        for line in fl:
+            data.append([float(i.strip()) for i in line.split(",")])
+    return data
+
+def main():
+    data1 = read_data(sys.argv[1])
+    data2  = read_data(sys.argv[2])
+
+    f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+
+    #plot accuracy vs feature count
+    x = [row[0] for row in data1]
+
+    f1_1, f1_2 = [row[1] for row in data1],[row[1] for row in data2]
+    ax1.set_title('Weighted F1-Score vs Feature Count')
+    ax1.scatter(x,f1_1, s=10, color = 'green', label = "Logistic Regression")
+    ax1.scatter(x,f1_2, s=10, color = 'blue', label = "KNN")
+    ax1.legend()
+
+    #plot F1-score
+    acc_1, acc_2 = [row[2] for row in data1],[row[2] for row in data2]
+    ax2.set_title('Accuracy vs Feature Count')
+    ax2.scatter(x,acc_1, s=10, color = 'green', label = "Logistic Regression")
+    ax2.scatter(x,acc_2, s=10, color = 'blue', label = "KNN")
+    ax2.legend()
+
+    f.subplots_adjust(hspace=0.3)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
